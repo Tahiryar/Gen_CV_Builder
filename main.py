5296c78945798886c76a037e82340e6decf3627c
Tahiryar <tahiryar.251k@gmail.com>
2025-06-23 14:31:01 +0500

    make data set for dataframe

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import os
 
 genai.configure(api_key="API_Key")
 
+data = {
+    'Name': ['Tahir', 'Ali', 'Zara'],
+    'Skills': ['Python, GenAI', 'Java, SQL', 'React, ML'],
+    'Experience': ['2 years in dev', '1 year backend', 'Intern in AI']
+}
+
+df = pd.DataFrame(data)
